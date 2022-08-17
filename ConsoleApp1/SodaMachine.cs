87a6e0a16cfec0ff83b8d9048e94becde9c7e954
Dominika Limanówka <47063149+xlimiii@users.noreply.github.com>
2022-08-17 22:42:25 +0200

    grouped all of the functions

--- ConsoleApp1/SodaMachine.cs
+++ ConsoleApp1/SodaMachine.cs
@@ -6,46 +6,100 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public enum SodaMachineChoice
-    {
-        InsertMoney = 1,
-        OrderSoda = 2,
-        OrderSmsSoda = 3,
-        Recall = 4
-    }
     public static class SodaMachine
     {
         private static int _money = 0;
         private static readonly Soda[] _inventory = new[] { new Soda { Name = "coke", Price = 20, Nr = 5 }, new Soda { Name = "sprite", Price = 20, Nr = 3 }, new Soda { Name = "fanta", Price = 20, Nr = 3 } };
 
-        private static void PrintInventory()
+        /// <summary>
+        /// This is the starter method for the machine
+        /// </summary>
+        public static void Start()
         {
-            foreach (var item in _inventory)
+            while (true)
             {
-                Console.WriteLine($"{item.Name} - {item.Price} NOK - ({item.Nr} available) ");
+                PrintMenu();
+                var userChoice = GetUserChoice();
+                switch (userChoice)
+                {
+                    case SodaMachineChoice.InsertMoney:
+                        var moneyInsertedByUser = GetUserMoney();
+                        InsertMoney(moneyInsertedByUser);
+                        break;
+                    case SodaMachineChoice.OrderSoda:
+                    case SodaMachineChoice.OrderSmsSoda:
+                        PrintInventory();
+                        var soda = GetUserSodaChoice();
+                        OrderProduct(soda, userChoice);
+                        break;
+                    case SodaMachineChoice.Recall:
+                        GiveMoneyBack();
+                        break;
+                    default:
+                        Console.Write("It's impossible to do that operation right now");
+                        break;
+                }
+                Console.Write("Press any button to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
+
+        #region main operation methods
         private static void InsertMoney(int count)
         {
             _money += count;
         }
-        private static void OrderProduct(Soda orderedSoda)
+
+        private static void OrderProduct(Soda orderedSoda, SodaMachineChoice choice) 
         {
-            if (CheckIfProductAvailable(orderedSoda) && CheckIfEnoughMoney(orderedSoda))
-            {
-                _money -= orderedSoda.Price;
-                orderedSoda.Nr -= 1;
-                Console.WriteLine($"Giving {orderedSoda.Name} out");
-                GiveMoneyBack();
-            }
-        }
-        private static void OrderProductViaSms(Soda orderedSoda)
-        {
+            bool isOrderSuccess = false;
             if (CheckIfProductAvailable(orderedSoda))
             {
+                switch (choice)
+                {
+                    case SodaMachineChoice.OrderSoda:
+                        isOrderSuccess = OrderProductFromMachine(orderedSoda);
+                        break;
+                    case SodaMachineChoice.OrderSmsSoda:
+                        isOrderSuccess = OrderProductViaSms(orderedSoda);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, there is no {orderedSoda.Name} left");
+            }
+
+            if (isOrderSuccess)
+            {
                 orderedSoda.Nr -= 1;
                 Console.WriteLine($"Giving {orderedSoda.Name} out");
             }
+            else
+            {
+                Console.WriteLine($"Order failed");
+            }
+        }
+        private static bool OrderProductFromMachine(Soda orderedSoda)
+        {
+            if (CheckIfEnoughMoney(orderedSoda))
+            {
+                _money -= orderedSoda.Price;
+                GiveMoneyBack();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static bool OrderProductViaSms(Soda orderedSoda)
+        {
+            Console.WriteLine($"Sms received - your order is processed");
+            return true;
         }
         private static void GiveMoneyBack()
         {
@@ -59,11 +113,15 @@ namespace ConsoleApp1
                 Console.WriteLine($"There is no money left in the machine");
             }
         }
-
+        #endregion
+        #region repository-like methods
         private static Soda GetSoda(string name)
         {
             return _inventory.Where(x => x.Name == name).FirstOrDefault();
         }
+        #endregion
+
+        #region validation methods
         private static bool CheckIfEnoughMoney(Soda chosenSoda)
         {
             if (_money >= chosenSoda.Price)
@@ -76,87 +134,71 @@ namespace ConsoleApp1
                 return false;
             }
         }
-        private static bool CheckIfProductAvailable(Soda chosenSoda)
+      
+        private static bool CheckIfProductAvailable(Soda chosenSoda) => chosenSoda.Nr > 0;
+        #endregion
+
+        #region simple printing methods
+        private static void PrintMenu()
         {
-            if (chosenSoda.Nr > 0)
+            Console.WriteLine("\n\nAvailable commands:");
+            Console.WriteLine("[1] Insert money - Money put into money slot");
+            Console.WriteLine($"[2] Order ({string.Join("/", _inventory.Where(x => x.Nr > 0).Select(x => x.Name))}) - Order from machines buttons");
+            Console.WriteLine($"[3] Sms order ({string.Join(" / ", _inventory.Where(x => x.Nr > 0).Select(x => x.Name))}) - Order sent by sms");
+            Console.WriteLine("[4] Recall - gives money back");
+            Console.WriteLine("-------");
+            Console.WriteLine("Inserted money: " + _money);
+            Console.WriteLine("-------\n\n");
+        }
+        private static void PrintInventory()
+        {
+            Console.WriteLine("\n\nAvailable sodas:");
+            foreach (var item in _inventory.Where(x => x.Nr > 0))
             {
-                return true;
-            }
-            else
-            {
-                Console.WriteLine($"Sorry, there is no {chosenSoda.Name} left");
-                return false;
+                Console.WriteLine($"{item.Name} - {item.Price} NOK - ({item.Nr} available) ");
             }
         }
+        #endregion
 
-
-        /// <summary>
-        /// This is the starter method for the machine
-        /// </summary>
-        public static void Start()
+        #region methods dealing with user input
+        private static SodaMachineChoice GetUserChoice()
         {
-
-            while (true)
+            Console.Write("Enter a number of your choice: ");
+            var input = Console.ReadLine();
+            SodaMachineChoice userChoice;
+            while (!(Enum.TryParse(input, true, out userChoice) && Enum.IsDefined(typeof(SodaMachineChoice), userChoice)))
             {
-                Console.WriteLine("\n\nAvailable commands:");
-                Console.WriteLine("[1] Insert money - Money put into money slot");
-                Console.WriteLine($"[2] Order ({string.Join("/", _inventory.Where(x => x.Nr > 0).Select(x => x.Name))}) - Order from machines buttons");
-                Console.WriteLine($"[3] Sms order ({string.Join(" / ", _inventory.Where(x => x.Nr > 0).Select(x => x.Name))}) - Order sent by sms");
-                Console.WriteLine("[4] Recall - gives money back");
-                Console.WriteLine("-------");
-                Console.WriteLine("Inserted money: " + _money);
-                Console.WriteLine("-------\n\n");
-                Console.Write("Enter a number of your choice: ");
-                var input = Console.ReadLine();
-                SodaMachineChoice userChoice;
-                while (!(Enum.TryParse(input, true, out userChoice) && Enum.IsDefined(typeof(SodaMachineChoice), userChoice)))
-                {
-                    Console.Write("Incorrect value. Enter a number of your choice: ");
-                    input = Console.ReadLine();
-                }
-                switch (userChoice)
-                {
-                    case SodaMachineChoice.InsertMoney:
-                        Console.Write("Enter an amount of money: ");
-                        var moneyAmountInput = Console.ReadLine();
-                        int moneyAmount;
-                        while (!int.TryParse(moneyAmountInput, out moneyAmount))
-                        {
-                            Console.Write("Incorrect value. Enter an amount of money: ");
-                            moneyAmountInput = Console.ReadLine();
-                        }
-                        InsertMoney(moneyAmount);
-                        break;
-                    case SodaMachineChoice.OrderSoda:
-                        PrintInventory();
-                        Console.Write("Enter a name of soda: ");
-                        var sodaNameInput = Console.ReadLine();
-                        while (GetSoda(sodaNameInput) == null)
-                        {
-                            Console.Write("Incorrect value. Enter a name of soda: ");
-                            sodaNameInput = Console.ReadLine();
-                        }
-                        OrderProduct(GetSoda(sodaNameInput));
-                        break;
-                    case SodaMachineChoice.OrderSmsSoda:
-                        PrintInventory();
-                        Console.Write("Enter a name of soda: ");
-                        sodaNameInput = Console.ReadLine();
-                        while (GetSoda(sodaNameInput) == null)
-                        {
-                            Console.Write("Incorrect value. Enter a name of soda: ");
-                            sodaNameInput = Console.ReadLine();
-                        }
-                        OrderProductViaSms(GetSoda(sodaNameInput));
-                        break;
-                    case SodaMachineChoice.Recall:
-                        GiveMoneyBack();
-                        break;
-                    default:
-                        Console.Write("It's impossible to do that operation right now");
-                        break;
-                }
+                Console.Write("Incorrect value. Enter a number of your choice: ");
+                input = Console.ReadLine();
             }
+            return userChoice;
         }
+        private static int GetUserMoney()
+        {
+            Console.Write("Enter an amount of money: ");
+            var moneyAmountInput = Console.ReadLine();
+            int moneyAmount;
+            while (!int.TryParse(moneyAmountInput, out moneyAmount))
+            {
+                Console.Write("Incorrect value. Enter an amount of money: ");
+                moneyAmountInput = Console.ReadLine();
+            }
+            return moneyAmount;
+        }
+        private static Soda GetUserSodaChoice()
+        {
+            Console.Write("Enter a name of soda: ");
+            var sodaNameInput = Console.ReadLine();
+            var soda = GetSoda(sodaNameInput);
+            while (soda == null)
+            {
+                Console.Write("Incorrect value. Enter a name of soda: ");
+                sodaNameInput = Console.ReadLine();
+                soda = GetSoda(sodaNameInput);
+            }
+            return soda;
+        }
+        #endregion
+
     }
 }
---
+++ ConsoleApp1/SodaMachineChoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum SodaMachineChoice
+    {
+        InsertMoney = 1,
+        OrderSoda = 2,
+        OrderSmsSoda = 3,
+        Recall = 4
+    }
+}
